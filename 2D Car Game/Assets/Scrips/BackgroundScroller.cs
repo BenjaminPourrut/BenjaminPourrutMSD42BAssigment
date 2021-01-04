2ab91ce0506f1bce4bfb079921d93860e98f455d
BenjaminPourrut <73096496+BenjaminPourrut@users.noreply.github.com>
2021-01-04 22:59:07 +0100

    Scrolling Background

---
+++ 2D Car Game/Assets/Scrips/BackgroundScroller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+
+    [SerializeField] float backgroundScrollSpeed = 0.02f;
+
+    Material MyOwnMaterial;
+
+    Vector2 offSet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyOwnMaterial = GetComponent<Renderer>().material;
+
+        offSet = new Vector2(0f, backgroundScrollSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MyOwnMaterial.mainTextureOffset += offSet * Time.deltaTime;
+    }
+}
